1c38a581419b35692e12d187b498a82f48872dcd
Randi Adiel Gianufian <randiadiel30@gmail.com>
2019-12-10 16:10:56 +0700

    Make Routes with the controller

---
+++ resources/views/create.blade.php
@@ -0,0 +1,54 @@
+<!-- create.blade.php -->
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Laravel 5.6 CRUD Tutorial With Example  </title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">  
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css" rel="stylesheet">  
+    <script  data-src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>  
+  </head>
+  <body>
+    <div class="container">
+      <h2>Randi Sucika System</h2><br/>
+      <form method="post" action="{{url('peoples')}}" enctype="multipart/form-data">
+        @csrf
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <label for="Name">Name:</label>
+            <input type="text" class="form-control" name="name">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Email">Messages:</label>
+              <textarea cols="50" type="text" class="form-control" name="messages"></textarea>
+            </div>
+          </div>
+        </div>
+         <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+                <lable>Role</lable>
+                <select name="role">
+                  <option value="Aktivis">Aktivis</option>
+                  <option value="Pengurus">Pengurus</option>
+                </select>
+            </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4" style="margin-top:60px">
+            <button type="submit" class="btn btn-success">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
