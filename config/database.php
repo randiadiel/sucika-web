e8dc2e956243d21b31dba28701cb1c9395fc2878
Randi Adiel Gianufian <randiadiel30@gmail.com>
2019-12-14 10:06:29 +0700

    Deployment Setup 2

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'aa121j392ocrkhn.c13mnc3djuog.ap-southeast-1.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'ebdb'),
+            'username' => env('DB_USERNAME', 'randiadiel'),
+            'password' => env('DB_PASSWORD', 'randi2000'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
--- public/index.php
+++ public/index.php
@@ -9,6 +9,12 @@
 
 define('LARAVEL_START', microtime(true));
 
+if(DB::connection()->getDatabaseName())
+{
+   echo "Connected to database ".DB::connection()->getDatabaseName();
+}
+$response->send();
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
